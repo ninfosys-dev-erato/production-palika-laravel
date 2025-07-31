c516e74d32164dce7bcd352cc344a0b46b52d018
kritisha <kritishamoktan530@gmail.com>
2025-07-31 10:25:04 +0545

    Updated business sidebar

--- resources/views/business-portal/business-sidebar.blade.php
+++ resources/views/business-portal/business-sidebar.blade.php
@@ -7,7 +7,7 @@
         </a>
     </li>
 
-    <li class="menu-item {{ \Illuminate\Support\Facades\Route::is('organization.dashboard') ? 'active' : '' }}">
+    <li class="menu-item {{ \Illuminate\Support\Facades\Route::is('organization.profile') ? 'active' : '' }}">
         <a href="{{ route('organization.profile') }}" class="menu-link">
             <i class="menu-icon tf-icons bx bx-home-circle"></i>
             <div data-i18n="Analytics">{{ __('Profile') }}</div>
