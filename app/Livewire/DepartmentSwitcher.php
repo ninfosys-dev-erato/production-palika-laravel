18de7dd66542358637917fc8a7ffccb4ad16ff17
kritisha <kritishamoktan530@gmail.com>
2025-08-02 23:22:41 +0545

    Updated department switcher

--- app/Livewire/DepartmentSwitcher.php
+++ app/Livewire/DepartmentSwitcher.php
@@ -4,6 +4,8 @@ namespace App\Livewire;
 
 use App\Facades\GlobalFacade;
 use Livewire\Component;
+use Illuminate\Support\Facades\Cache;
+use Carbon\Carbon;
 
 class DepartmentSwitcher extends Component
 {
@@ -15,17 +17,22 @@ class DepartmentSwitcher extends Component
     {
         $this->ward = GlobalFacade::ward();
 
+        // Set default department if not already set
         if (!session()->has('department')) {
             $defaultDepartment = auth()->user()->departments()->first();
             if ($defaultDepartment) {
                 session(['department' => $defaultDepartment->id]);
+                // Clear the cache to force refresh
+                Cache::forget('user_departments_' . auth()->id());
             }
         }
     }
 
-    public function changeDepartment(int $ward)
+    public function changeDepartment(int $department)
     {
-       GlobalFacade::department($ward);
+        GlobalFacade::department($department);
+        // Clear the cache to force refresh
+        Cache::forget('user_departments_' . auth()->id());
         $this->dispatch('department-change');
     }
 
