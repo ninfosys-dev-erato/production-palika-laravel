f52d5b31c4cd4f84340fcc491d9538b71ee01bb0
NabinNS <nabinshrestha348@gmail.com>
2025-08-12 15:09:10 +0545

    route fix

--- src/Beruju/routes/beruju-entry.php
+++ src/Beruju/routes/beruju-entry.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 use Src\Beruju\Controllers\BerujuEntryAdminController;
 use Src\Beruju\Controllers\BerujuDashboardController;
 
-Route::group(['prefix' => 'admin/beruju', 'as' => 'admin.beruju.registration.', 'middleware' => ['web', 'auth']], function () {
+Route::group(['prefix' => 'admin/beruju/registration', 'as' => 'admin.beruju.registration.', 'middleware' => ['web', 'auth']], function () {
     Route::get('/', [BerujuEntryAdminController::class, 'index'])->name('index');
     Route::get('/create', [BerujuEntryAdminController::class, 'create'])->name('create');
     Route::get('/edit/{id}', [BerujuEntryAdminController::class, 'edit'])->name('edit');
