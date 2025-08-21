11b32a1dfbd396e547d7925f1c47769a101ed351
NabinNS <nabinshrestha348@gmail.com>
2025-08-21 15:50:15 +0545

    [ Ejalas ] feat: improvised enum

--- src/Ejalas/Enum/ElectedPosition.php
+++ src/Ejalas/Enum/ElectedPosition.php
@@ -4,8 +4,69 @@ namespace Src\Ejalas\Enum;
 
 enum ElectedPosition: string
 {
-    case deputyMayor = 'Deputy Mayor';
-    case wardPresident = 'Ward President';
-    case executiveMember = 'Executive Member';
-    case wardMember = 'Ward Member';
+    case DEPUTY_MAYOR = 'deputy_mayor';
+    case WARD_PRESIDENT = 'ward_president';
+    case EXECUTIVE_MEMBER = 'executive_member';
+    case WARD_MEMBER = 'ward_member';
+
+    public function label(): string
+    {
+        return self::getLabel($this);
+    }
+
+    public static function getLabel(self $value): string
+    {
+        return match ($value) {
+            self::DEPUTY_MAYOR => __('Deputy Mayor'),
+            self::WARD_PRESIDENT => __('Ward President'),
+            self::EXECUTIVE_MEMBER => __('Executive Member'),
+            self::WARD_MEMBER => __('Ward Member'),
+        };
+    }
+
+    public static function getValuesWithLabels(): array
+    {
+        $valuesWithLabels = [];
+
+        foreach (self::cases() as $value) {
+            $valuesWithLabels[] = [
+                'value' => $value,
+                'label' => $value->label(),
+            ];
+        }
+
+        return $valuesWithLabels;
+    }
+
+    public static function getForWeb(): array
+    {
+        $valuesWithLabels = [];
+
+        foreach (self::cases() as $value) {
+            $valuesWithLabels[$value->value] = $value->label();
+        }
+
+        return $valuesWithLabels;
+    }
+
+    public static function getNepaliLabel(self $value): string
+    {
+        return match ($value) {
+            self::DEPUTY_MAYOR => 'उपमहापौर',
+            self::WARD_PRESIDENT => 'वडा अध्यक्ष',
+            self::EXECUTIVE_MEMBER => 'कार्यकारी सदस्य',
+            self::WARD_MEMBER => 'वडा सदस्य',
+        };
+    }
+
+    public static function getForWebInNepali(): array
+    {
+        $valuesWithLabels = [];
+
+        foreach (self::cases() as $value) {
+            $valuesWithLabels[$value->value] = self::getNepaliLabel($value);
+        }
+
+        return $valuesWithLabels;
+    }
 }
