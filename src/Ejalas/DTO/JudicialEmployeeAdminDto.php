45ede2a36c9b637c1eed5d49d887eec24e79471b
NabinNS <nabinshrestha348@gmail.com>
2025-08-21 15:51:41 +0545

    [ Ejalas ] fix: fixed crud for judicial employee

--- src/Ejalas/DTO/JudicialEmployeeAdminDto.php
+++ src/Ejalas/DTO/JudicialEmployeeAdminDto.php
@@ -6,27 +6,26 @@ use Src\Ejalas\Models\JudicialEmployee;
 
 class JudicialEmployeeAdminDto
 {
-   public function __construct(
+    public function __construct(
         public string $name,
-        public string $local_level_id,
         public string $ward_id,
         public string $level_id,
         public string $designation_id,
         public string $join_date,
         public string $phone_no,
         public string $email
-    ){}
+    ) {}
 
-public static function fromLiveWireModel(JudicialEmployee $judicialEmployee):JudicialEmployeeAdminDto{
-    return new self(
-        name: $judicialEmployee->name,
-        local_level_id: $judicialEmployee->local_level_id,
-        ward_id: $judicialEmployee->ward_id,
-        level_id: $judicialEmployee->level_id,
-        designation_id: $judicialEmployee->designation_id,
-        join_date: $judicialEmployee->join_date,
-        phone_no: $judicialEmployee->phone_no,
-        email: $judicialEmployee->email
-    );
-}
+    public static function fromLiveWireModel(JudicialEmployee $judicialEmployee): JudicialEmployeeAdminDto
+    {
+        return new self(
+            name: $judicialEmployee->name,
+            ward_id: $judicialEmployee->ward_id,
+            level_id: $judicialEmployee->level_id,
+            designation_id: $judicialEmployee->designation_id,
+            join_date: $judicialEmployee->join_date,
+            phone_no: $judicialEmployee->phone_no,
+            email: $judicialEmployee->email
+        );
+    }
 }
