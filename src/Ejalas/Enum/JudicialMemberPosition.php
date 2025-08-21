586c0e501528fa3f13a9c0ea7ee06fc143460c0d
NabinNS <nabinshrestha348@gmail.com>
2025-08-21 15:50:15 +0545

    [ Ejalas ] feat: improvised enum

--- src/Ejalas/Enum/JudicialMemberPosition.php
+++ src/Ejalas/Enum/JudicialMemberPosition.php
@@ -4,6 +4,63 @@ namespace Src\Ejalas\Enum;
 
 enum JudicialMemberPosition: string
 {
-    case coordinator = 'Coordinator';
-    case member = 'Member';
+    case COORDINATOR = 'coordinator';
+    case MEMBER = 'member';
+
+    public function label(): string
+    {
+        return self::getLabel($this);
+    }
+
+    public static function getLabel(self $value): string
+    {
+        return match ($value) {
+            self::COORDINATOR => __('Coordinator'),
+            self::MEMBER => __('Member'),
+        };
+    }
+
+    public static function getValuesWithLabels(): array
+    {
+        $valuesWithLabels = [];
+
+        foreach (self::cases() as $value) {
+            $valuesWithLabels[] = [
+                'value' => $value,
+                'label' => $value->label(),
+            ];
+        }
+
+        return $valuesWithLabels;
+    }
+
+    public static function getForWeb(): array
+    {
+        $valuesWithLabels = [];
+
+        foreach (self::cases() as $value) {
+            $valuesWithLabels[$value->value] = $value->label();
+        }
+
+        return $valuesWithLabels;
+    }
+
+    public static function getNepaliLabel(self $value): string
+    {
+        return match ($value) {
+            self::COORDINATOR => 'समन्वयक',
+            self::MEMBER => 'सदस्य',
+        };
+    }
+
+    public static function getForWebInNepali(): array
+    {
+        $valuesWithLabels = [];
+
+        foreach (self::cases() as $value) {
+            $valuesWithLabels[$value->value] = self::getNepaliLabel($value);
+        }
+
+        return $valuesWithLabels;
+    }
 }
