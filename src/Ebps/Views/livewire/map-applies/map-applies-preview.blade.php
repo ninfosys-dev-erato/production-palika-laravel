5338107306a0215669f3bb7d5bcbc37d0e2818f0
kritisha <kritishamoktan530@gmail.com>
2025-09-05 17:27:22 +0545

    updated the test template view

--- src/Ebps/Views/livewire/map-applies/map-applies-preview.blade.php
+++ src/Ebps/Views/livewire/map-applies/map-applies-preview.blade.php
@@ -40,7 +40,7 @@
                     @foreach ($step->mapApplyStepTemplates as $letter)
                         @if ($letter->form_id)
                             <div class="d-flex justify-content-end mb-3">
-                                <button type="button" class="btn btn-danger" wire:click="print({{ $letter->id }})"
+                                <button type="button" class="btn btn-danger" onclick="printDiv()"
                                     data-bs-toggle="tooltip" data-bs-placement="top" title="Print">
                                     <i class="bx bx-printer"></i> {{ __('ebps::ebps.print') }}
                                 </button>
@@ -304,5 +304,57 @@
             </div>
         </div>
     @endif
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/html2canvas/1.4.1/html2canvas.min.js"></script>
+
+    {{-- this script lets user download the pdf --}}
+    <script>
+        async function printDiv() {
+            const {
+                jsPDF
+            } = window.jspdf;
+            const element = document.getElementById('printContent');
+
+
+            const canvas = await html2canvas(element, {
+                scale: 2,
+                useCORS: true,
+            });
+
+            const imgData = canvas.toDataURL('image/png');
+            const pdf = new jsPDF('p', 'mm', 'a4');
+
+            const pdfWidth = pdf.internal.pageSize.getWidth();
+            const pdfHeight = pdf.internal.pageSize.getHeight();
+
+            const imgProps = pdf.getImageProperties(imgData);
+            const imgHeight = (imgProps.height * pdfWidth) / imgProps.width;
+
+            let heightLeft = imgHeight;
+            let position = 0;
+
+            pdf.addImage(imgData, 'PNG', 0, position, pdfWidth, imgHeight);
+            heightLeft -= pdfHeight;
+
+            // Only add more pages if the image is taller than one page
+            while (heightLeft > 1) {
+                position -= pdfHeight;
+                pdf.addPage();
+                pdf.addImage(imgData, 'PNG', 0, position, pdfWidth, imgHeight);
+                heightLeft -= pdfHeight;
+            }
+            pdf.save("certificate.pdf");
+        }
+        // Listen for Livewire print event
+        document.addEventListener('livewire:init', () => {
+            Livewire.on('print-certificate-letter', () => {
+
+                printDiv();
+            });
+
+
+        });
+    </script>
 
 </div>
