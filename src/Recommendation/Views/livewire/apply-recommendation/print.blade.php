1cf441dd1aad222577923d1bf766dd0da080ac82
Nabin Shrestha <nabinshrestha348@gmail.com>
2025-10-27 04:23:37 +0000

    fix summernote and recommendation print

--- src/Recommendation/Views/livewire/apply-recommendation/print.blade.php
+++ src/Recommendation/Views/livewire/apply-recommendation/print.blade.php
@@ -1,65 +1,64 @@
 <div class="row">
 
     <div class="col-md-12">
-    <div class="d-flex align-items-center gap-2 flex-wrap w-100 justify-content-between">
-        <!-- Left-aligned buttons -->
-        <div class="d-flex align-items-center gap-2 flex-wrap">
-            <button class="btn btn-outline-primary" type="submit" wire:loading.attr="disabled" wire:click="save">
-                <i class="bx bx-save"></i> {{ __('recommendation::recommendation.save') }}
-            </button>
-            <button class="btn btn-outline-primary" type="submit" wire:loading.attr="disabled" wire:click="resetLetter">
-                <i class="bx bx-reset"></i> {{ __('recommendation::recommendation.reset') }}
-            </button>
-            <div class="d-flex align-items-center">
-                <label for="" class="mb-0">{{ __('recommendation::recommendation.edit_mode') }}&nbsp;</label>
-                <div class="form-check form-switch mb-0">
-                    <input type="checkbox" class="form-check-input" {{ !$preview ? 'checked' : '' }}
-                        wire:click="togglePreview">
+        <div class="d-flex align-items-center gap-2 flex-wrap w-100 justify-content-between">
+            <!-- Left-aligned buttons -->
+            <div class="d-flex align-items-center gap-2 flex-wrap">
+                <button class="btn btn-outline-primary" type="submit" wire:loading.attr="disabled" wire:click="save">
+                    <i class="bx bx-save"></i> {{ __('recommendation::recommendation.save') }}
+                </button>
+                <button class="btn btn-outline-primary" type="submit" wire:loading.attr="disabled"
+                    wire:click="resetLetter">
+                    <i class="bx bx-reset"></i> {{ __('recommendation::recommendation.reset') }}
+                </button>
+                <div class="d-flex align-items-center">
+                    <label for=""
+                        class="mb-0">{{ __('recommendation::recommendation.edit_mode') }}&nbsp;</label>
+                    <div class="form-check form-switch mb-0">
+                        <input type="checkbox" class="form-check-input" {{ !$preview ? 'checked' : '' }}
+                            wire:click="togglePreview">
+                    </div>
                 </div>
             </div>
-        </div>
 
-        <div>
-            @if($applyRecommendation->status == Src\Recommendation\Enums\RecommendationStatusEnum::ACCEPTED)
-            <button type="button" class="btn btn-info" onclick="printRecDiv()"
-                data-bs-toggle="tooltip" data-bs-placement="top" title="Print">
-                <i class="bx bx-printer"></i> {{ __('recommendation::recommendation.print') }}
-            </button>
-            @endif
+            <div>
+                @if ($applyRecommendation->status == Src\Recommendation\Enums\RecommendationStatusEnum::ACCEPTED)
+                    <button type="button" class="btn btn-info" onclick="printRecDiv()" data-bs-toggle="tooltip"
+                        data-bs-placement="top" title="Print">
+                        <i class="bx bx-printer"></i> {{ __('recommendation::recommendation.print') }}
+                    </button>
+                @endif
+            </div>
         </div>
     </div>
-</div>
-
 
     <div class="col-md-12 {{ $preview ? 'd-none' : '' }}">
-{{--        <x-form.ck-editor-input label="" id="recommendation_letter" name="letter" :value="$letter" />--}}
-        <x-form.summernote-editor label="" id="recommendation_letter" name="letter" :value="$letter"/>
+        {{--        <x-form.ck-editor-input label="" id="recommendation_letter" name="letter" :value="$letter" /> --}}
+        <x-form.summernote-editor label="" id="recommendation_letter" name="letter" :value="$letter" />
     </div>
 
-
-
     <div class="col-md-12 {{ !$preview ? 'd-none' : '' }}">
-            <div style="border-radius: 10px; text-align: center; padding: 20px;">
-                <div id="printContent" class="a4-container">
+        <div style="border-radius: 10px; text-align: center; padding: 20px;">
+            <div id="printContent" class="a4-container">
                 {!! $letter . $styles !!}
                 @php
-                $watermark = getSetting('palika-campaign-logo');
-            @endphp
+                    $watermark = getSetting('palika-campaign-logo');
+                @endphp
 
-            @if (!empty($watermark) && getSetting('show-letter-watermark'))
-                <img class="watermark" src="{{ $watermark }}" alt="Watermark">
-            @endif
-                </div>
+                @if (!empty($watermark) && getSetting('show-letter-watermark'))
+                    <img class="watermark" src="{{ $watermark }}" alt="Watermark">
+                @endif
             </div>
         </div>
+    </div>
 
-        <style>
-         @font-face {
-        font-family: 'Kalimati';
-        src: url('/fonts/Kalimati.ttf') format('truetype');
-        font-weight: normal;
-        font-style: normal;
-    }
+    <style>
+        @font-face {
+            font-family: 'Kalimati';
+            src: url('/fonts/Kalimati.ttf') format('truetype');
+            font-weight: normal;
+            font-style: normal;
+        }
 
         /* Ensure A4 Size */
         .a4-container {
@@ -76,20 +75,21 @@
             color: #000;
         }
 
+        .watermark {
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%, -50%);
+            opacity: 0.1;
+            /* Adjust transparency here */
+            width: 60%;
+            height: 40%;
+            z-index: 0;
+            /* Ensure it stays behind text */
+            pointer-events: none;
+            /* So it doesn't interfere with interaction */
+        }
 
-
-     .watermark {
-        position: absolute;
-        top: 50%;
-        left: 50%;
-        transform: translate(-50%, -50%);
-        opacity: 0.1; /* Adjust transparency here */
-        width: 60%;
-        height: 40%;
-        z-index: 0; /* Ensure it stays behind text */
-        pointer-events: none; /* So it doesn't interfere with interaction */
-    }
-      
         /* Print Styling */
         @media print {
             body {
@@ -111,23 +111,21 @@
         }
     </style>
 
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/html2canvas/1.4.1/html2canvas.min.js"></script>
 
     {{-- this script lets user download the pdf --}}
+
     <script>
         async function printRecDiv() {
-            const timestamp = new Date().getTime();
-
             const {
                 jsPDF
             } = window.jspdf;
             const element = document.getElementById('printContent');
 
-
             const canvas = await html2canvas(element, {
                 scale: 2,
-                useCORS: true,
+                useCORS: true
             });
 
             const imgData = canvas.toDataURL('image/png');
@@ -142,18 +140,21 @@
             let heightLeft = imgHeight;
             let position = 0;
 
+            // Add first page
             pdf.addImage(imgData, 'PNG', 0, position, pdfWidth, imgHeight);
             heightLeft -= pdfHeight;
 
-            // Only add more pages if the image is taller than one page
+            // Add more pages only if needed
             while (heightLeft > 1) {
                 position -= pdfHeight;
                 pdf.addPage();
                 pdf.addImage(imgData, 'PNG', 0, position, pdfWidth, imgHeight);
                 heightLeft -= pdfHeight;
             }
-            pdf.save(`certificate_${timestamp}.pdf`);
+
+            // Trigger browser print dialog
+            pdf.autoPrint();
+            window.open(pdf.output('bloburl'), '_blank');
         }
     </script>
-
 </div>
